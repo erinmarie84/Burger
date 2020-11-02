27cdf34e256e31a3aaac7f6a429d8ba291fc552f
Erin Frasier <e_marie84@ymail.com>
2020-11-02 15:41:41 -0800

    fixing bugs

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,3 @@
-
-// Set up MySQL connection.
 var mysql = require("mysql");
 
 var connection;
@@ -14,7 +12,6 @@ if (process.env.JAWSDB_URL){
 });
 }
 
-// Make connection.
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
@@ -23,5 +20,4 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
-// Export connection for our ORM to use.
 module.exports = connection;
